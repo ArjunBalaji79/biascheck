4b2e65646a14ee53aa42f637eac539191eb60d26
Arjun Balaji <95492772+ArjunBalaji79@users.noreply.github.com>
2024-11-19 16:14:55 +0530

    better examples and added __init__ for all folders

--- biascheck/__init__.py
+++ biascheck/__init__.py
@@ -2,5 +2,6 @@ from .analysis.docucheck import DocuCheck
 from .analysis.moducheck import ModuCheck
 from .analysis.setcheck import SetCheck
 from .analysis.basecheck import BaseCheck
+from .analysis.ragcheck import RAGCheck
 
-__all__ = ["DocuCheck", "ModuCheck", "SetCheck", "BaseCheck"]
+__all__ = ["DocuCheck", "ModuCheck", "SetCheck", "BaseCheck", "RAGCheck"]
--- setup.py
+++ setup.py
@@ -1,29 +1,44 @@
 from setuptools import setup, find_packages
 
+import subprocess
+subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"], check=True)
+
 setup(
     name="biascheck",
-    version="0.1.0",
-    description="An open-source library for checking bias in documents, models, datasets, and databases.",
+    version="0.2.0",
     author="Arjun Balaji",
-    author_email="arjun.balaji.02@example.com",
-    url="https://github.com/ArjunBalaji79/biascheck",
+    author_email="",
+    description="A library for detecting and analyzing bias in text, datasets, and language models.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/your-repo/biascheck",
     packages=find_packages(),
+    python_requires=">=3.9,<3.11",
     install_requires=[
-        "transformers",
+        "numpy"
         "torch",
-        "faiss-cpu",
-        "spacy",
+        "transformers",
+        "langchain",
         "pandas",
         "matplotlib",
-        "wordcloud",
-        "click",
         "scikit-learn",
-        "pymupdf",
-        "langchain",
+        "PyPDF2",
+        "textblob",
+        "wordcloud",
+        "faiss-cpu",
+        "sentence-transformers",
+        "PyMuPDF",
+        "spacy",
+        "scipy",
+        "langchain-community",
+        "seaborn",
+        
     ],
+
     entry_points={
         "console_scripts": [
-            "biascheck=cli:cli",
-        ]
+            "biascheck=biascheck.cli:main",
+        ],
     },
+
 )
